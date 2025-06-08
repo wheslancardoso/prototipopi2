338d6cd00abfa17646c784e0f5eb6a91ccfa9191
wheslancardoso <wheslancardoso1@gmail.com>
2025-06-08 10:46:39 -0300

    refactor: remove classes de estado de poltrona obsoletas
    - Exclui as classes DisponivelState, OcupadaState, PoltronaState e SelecionadaState, que não são mais necessárias na lógica atual do sistema.
    - Essa remoção simplifica a estrutura do código e melhora a manutenção futura.

--- src/main/java/com/teatro/model/PoltronaState.java
+++
@@ -1,8 +0,0 @@
-package com.teatro.model;
-
-public interface PoltronaState {
-    void ocupar(Poltrona poltrona);
-    void liberar(Poltrona poltrona);
-    void selecionar(Poltrona poltrona);
-    String getNomeEstado();
-} 
